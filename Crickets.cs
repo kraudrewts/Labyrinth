8bf617bc8a635d1b26b0d8e3c56b16ff23c5b4e3
kraudrewts <andreiciske@gmail.com>
2025-03-03 04:03:09 +0500

    Add files via upload
    Crickets script Unity

---
+++ Crickets.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Выводим слово "crickets" в консоль каждый кадр
+        Debug.Log("crickets");
+        
+    }
+}
